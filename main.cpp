5911a9a5cfc42e00d02a3d461dd9743b098e7678
Lucas Solomon <lucas_solomon@cs.ucsb.edu>
2024-11-14 18:43:43 -0800

    added false positive rate

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <queue>
 #include <cmath>
 #include <unordered_map> 
+#include <cmath>
+#include <random>
 
 #include "MHashMap.h"
 using namespace std;
@@ -12,6 +14,9 @@ using namespace std;
 
 
 int main(){
+
+	/*
+	FOR TESTING
 	//cout << "starting main" <<endl;
 	// N will be (2^31)-2
 	int N=(pow(2,31))-2;
@@ -33,5 +38,36 @@ int main(){
 	else{
 		cout << "Hash doesn't contains 12345"  << endl;
 	}
+	*/
+
+	// N will be (2^31)-2
+	int N=(pow(2,31))-2;
+	//n is 100,000
+	int n=500000;
+	// c  will range from 5-10
+	//k will range from 3-7
+	int c=5;
+	int k=3;
+	bool prime =false;
+
+	MHashMap myHash(prime, N, n, c, k);
+	for(int i=0; i<n; i++){
+		myHash.mapVal(i);
+	}
+	for(int )
+	mt19937 mt(time(nullptr));
+	std::uniform_int_distribution<> uniform_dist(n, N-1);
+	double fp=0;
+	double inserts=10000;
+	for(int i=0; i<inserts; i++){
+		if (myHash.contains(uniform_dist(mt))){
+			cout << "a false positive" << endl;
+			fp++;
+		}
+	}
+	double fpr=fp/inserts;
+	cout << "False positive rate is: " << fpr << " or " << fpr*100 << "%" << endl;
+
+
 	return 0;
 }
